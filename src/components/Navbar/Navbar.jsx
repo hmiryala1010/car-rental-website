37225f7c443f95ff8ee45e6bbe96023e09909df0
Hasini Miryala <hasini.miryala7585@gmail.com>
2025-02-20 15:23:25 -0600

    added responsive menu bar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -15,45 +15,67 @@ export const Navlinks = [
     link: "/#cars",
   },
   {
-    id: 1,
+    id: 3,
     name: "ABOUT",
     link: "/#about",
   },
   {
-    id: 1,
+    id: 4,
     name: "BOOKING",
     link: "/#booking",
   },
 ];
+
 const Navbar = ({ theme, setTheme }) => {
   const [showMenu, setShowMenu] = useState(false);
 
   const toggleMenu = () => {
     setShowMenu(!showMenu);
   };
+
   return (
     <div
-      className="relative z-10 shadow-md w-full dark:bg-black dark:text-white duration-300
-    "
+      className="relative z-10 shadow-md w-full dark:bg-black dark:text-white duration-300"
     >
       <div className="container py-2 md:py-0">
         <div className="flex justify-between items-center">
+          {/* Mobile view Hamburger icon on the left */}
+          <div className="flex items-center gap-4 md:hidden">
+            {/* Mobile Hamburger icon */}
+            {showMenu ? (
+              <HiMenuAlt1
+                onClick={toggleMenu}
+                className="cursor-pointer transition-all"
+                size={30}
+              />
+            ) : (
+              <HiMenuAlt3
+                onClick={toggleMenu}
+                className="cursor-pointer transition-all"
+                size={30}
+              />
+            )}
+          </div>
+
+          {/* Logo */}
           <div>
             <span className="text-3xl font-bold font-serif">Car Rental</span>
           </div>
+
+          {/* Desktop nav links */}
           <nav className="hidden md:block">
             <ul className="flex items-center gap-8">
               {Navlinks.map(({ id, name, link }) => (
                 <li key={id} className="py-4">
                   <a
                     href={link}
-                    className=" text-lg font-medium  hover:text-primary py-2 hover:border-b-2 hover:border-primary transition-colors duration-500  "
+                    className="text-lg font-medium hover:text-primary py-2 hover:border-b-2 hover:border-primary transition-colors duration-500"
                   >
                     {name}
                   </a>
                 </li>
               ))}
-              {/* DarkMode feature implement */}
+              {/* DarkMode feature */}
               {theme === "dark" ? (
                 <BiSolidSun
                   onClick={() => setTheme("light")}
@@ -67,9 +89,10 @@ const Navbar = ({ theme, setTheme }) => {
               )}
             </ul>
           </nav>
-          {/* Mobile view  */}
-          <div className="flex items-center gap-4 md:hidden ">
-            {/* dark  mode */}
+
+          {/* Mobile view Dark Mode Icon */}
+          <div className="flex items-center gap-4 md:hidden">
+            {/* dark mode toggle */}
             {theme === "dark" ? (
               <BiSolidSun
                 onClick={() => setTheme("light")}
@@ -79,20 +102,6 @@ const Navbar = ({ theme, setTheme }) => {
               <BiSolidMoon
                 onClick={() => setTheme("dark")}
                 className="text-2xl"
-              />
-            )}
-            {/* Mobile Hamburger icon */}
-            {showMenu ? (
-              <HiMenuAlt1
-                onClick={toggleMenu}
-                className=" cursor-pointer transition-all"
-                size={30}
-              />
-            ) : (
-              <HiMenuAlt3
-                onClick={toggleMenu}
-                className="cursor-pointer transition-all"
-                size={30}
               />
             )}
           </div>
--- src/components/Navbar/ResponsiveMenu.jsx
+++ src/components/Navbar/ResponsiveMenu.jsx
@@ -31,11 +31,7 @@ const ResponsiveMenu = ({ showMenu }) => {
           </ul>
         </nav>
       </div>
-      <div className="footer">
-        <h1>
-          Made with ❤ by <a href="https://dilshad-ahmed.github.io/">Dilshad</a>{" "}
-        </h1>
-      </div>
+     
     </div>
   );
 };
